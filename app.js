f63997ff8ca2a23f1111a0c65820e71d78e6ae8d
Erick <erick@MacBook-Air-de-Erick.local>
2026-06-18 19:12:57 -0600

    🔧 Ajuste: Archivo .htaccess blindado para Hostinger

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-require('./server.js'
+// 🟢 INYECCIÓN MAESTRA: Forzamos al servidor a tener la contraseña de tu base de datos en su memoria
+process.env.DATABASE_URL = "mysql://u593054134_catalogo:AdminTrendy2026*@31.97.208.160:3306/u593054134_catalogo";
+
+// Arrancamos tu servidor tradicional
+require('./server.js');
