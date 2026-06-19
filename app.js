9acdfc5c4bb0964e874b03c354debd01968b7f91
Erick <erick@MacBook-Air-de-Erick.local>
2026-06-18 19:28:24 -0600

    🔧 Ajuste: Archivo .htaccess blindado para Hostinger

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // 1. 🟢 INYECCIÓN MAESTRA: Clavamos la contraseña en el corazón de Node para que Prisma no se pierda
-process.env.DATABASE_URL = "mysql://u593054134_catalogo:AdminTrendy2026*@31.97.208.160:3306/u593054134_catalogo";
+process.env.DATABASE_URL = "mysql://u593054134_catalogo:Pastelita1!@31.97.208.160:3306/u593054134_catalogo";
 
 // 2. 🛡️ CAPTURA DE ERRORES: Evitamos que el servidor tire Error 500 si hay una consulta colgada en la red
 process.on('unhandledRejection', (reason, promise) => {
